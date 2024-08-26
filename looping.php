a376f9248260cd980eac1f21ddbe27677c7a5eb3
ProjectRavel <rafaelcodeid@gmail.com>
2024-08-27 00:57:18 +0700

    ADd Looping.php

---
+++ looping.php
@@ -0,0 +1,17 @@
+<?php
+
+$min = 0;
+$max = 100;
+
+// While
+
+// while($min <= $max){
+//     echo "Angka: " . $min . "<br>";
+//     $min++;
+// }
+
+for($min; $min <= $max; $min++){
+    echo "Angka: ". $min. "<br />";
+}
+
+?>
