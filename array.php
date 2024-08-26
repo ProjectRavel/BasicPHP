ae3952eb1d6e4c1615b72aa38c00c71bc37f8e47
ProjectRavel <rafaelcodeid@gmail.com>
2024-08-27 01:09:52 +0700

    Add array.php

---
+++ array.php
@@ -0,0 +1,10 @@
+<?php
+
+    $db_cashier = [
+        "user" => ["vels", "pelski"],
+        "password" => ["123", "321"],
+        "cashier_id" => [1.1, 1.2],
+    ];
+
+    echo $db_cashier["user"][0];
+?>  
