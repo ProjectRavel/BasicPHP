8363868fc8364dc7abf7550c3b39b49e43039c40
ProjectRavel <rafaelcodeid@gmail.com>
2024-09-01 19:33:00 +0700

    Add superglobal.php

---
+++ php/superglobal.php
@@ -0,0 +1,14 @@
+<?php
+
+/* --------------- SuperGlobals --------------------------------
+// built in variables that are always avlable in all scopers
+
+/*
+$_GET = contains information about variables passed through a URL or a form
+$_POST = contains information about variables passed through a form
+$_COOKIE = Contains information about variables passed through a cookie
+$_SESSION = Contains information about variables passed through a session
+$_SERVER = Contains information about the server environment
+$_ENV = Contains information about the environment variables
+$_FILES = contains information about filed uploaded to the script
+$_REQUEST = contains information about variables passed through a form of URL 
