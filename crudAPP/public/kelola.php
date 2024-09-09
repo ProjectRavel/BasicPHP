9030475d377b0de551167e057c6ecc568e86d8d8
ProjectRavel <rafaelcodeid@gmail.com>
2024-09-09 23:19:19 +0700

    Add Handling Structure & Create proses.php

--- crudAPP/public/kelola.php
+++ crudAPP/public/kelola.php
@@ -6,57 +6,63 @@ include '../component/navbar.php';
 
 <div class="container mt-5">
   <h2 class="mb-4 fw-bold text-primary">Tambah/Edit Data Siswa</h2>
-  <!-- NISN -->
-  <div class="mb-3">
-    <label for="nisn" class="form-label">NISN</label>
-    <input
-      type="text"
-      id="nisn"
-      class="form-control"
-      placeholder="Masukkan NISN..." />
-  </div>
-  <!-- Nama -->
-  <div class="mb-3">
-    <label for="Nama" class="form-label">Nama</label>
-    <input
-      type="text"
-      id="Nama"
-      class="form-control"
-      placeholder="Masukkan Nama Siswa/i" />
-  </div>
-  <!-- Jenis Kelamin -->
-  <div class="mb-3">
-    <label for="jeniskelamin" class="form-label">Jenis Kelamin</label>
-    <select id="jeniskelamin" class="form-select">
-      <option selected>Pilih Jenis Kelamin</option>
-      <option value="1">Laki-laki</option>
-      <option value="2">Perempuan</option>
-    </select>
-  </div>
-  <!-- Foto Siswa -->
-  <div class="mb-3">
-    <label for="fotosiswa" class="form-label">Pilih Foto Siswa</label>
-    <input class="form-control" type="file" id="fotosiswa" />
-  </div>
-  <!-- Alamat -->
-  <div class="mb-3">
-    <label for="alamat" class="form-label">Alamat</label>
-    <textarea class="form-control" id="alamat" rows="3"></textarea>
-  </div>
-  <div class="d-flex gap-2">
-    <?php 
-      if(isset($_GET['ubah'])){
-        ?>
-    <a type="button" class="btn btn-primary"><i class="fa-solid fa-check"></i> Ubah Data</a>
+
+  <form action="proses.php" method="post">
+    <div class="formulir">
+      <!-- NISN -->
+      <div class="mb-3">
+        <label for="nisn" class="form-label">NISN</label>
+        <input
+          type="text"
+          id="nisn"
+          class="form-control"
+          placeholder="Masukkan NISN..." />
+      </div>
+      <!-- Nama -->
+      <div class="mb-3">
+        <label for="Nama" class="form-label">Nama</label>
+        <input
+          type="text"
+          id="Nama"
+          class="form-control"
+          placeholder="Masukkan Nama Siswa/i" />
+      </div>
+      <!-- Jenis Kelamin -->
+      <div class="mb-3">
+        <label for="jeniskelamin" class="form-label">Jenis Kelamin</label>
+        <select id="jeniskelamin" class="form-select">
+          <option selected>Pilih Jenis Kelamin</option>
+          <option value="1">Laki-laki</option>
+          <option value="2">Perempuan</option>
+        </select>
+      </div>
+      <!-- Foto Siswa -->
+      <div class="mb-3">
+        <label for="fotosiswa" class="form-label">Pilih Foto Siswa</label>
+        <input class="form-control" type="file" id="fotosiswa" />
+      </div>
+      <!-- Alamat -->
+      <div class="mb-3">
+        <label for="alamat" class="form-label">Alamat</label>
+        <textarea class="form-control" id="alamat" rows="3"></textarea>
+      </div>
+      <div class="d-flex gap-2">
         <?php
-      } else {
-       ?>
-    <a type="button" class="btn btn-primary"><i class="fa-solid fa-plus"></i> Tambahkan Data</a>
-        <?php
-      }
+        if (isset($_GET['ubah'])) {
         ?>
-    <a type="button" class="btn btn-danger" href="index.php"><i class="fa-solid fa-xmark"></i> Batal</a>
-  </div>
+          <button type="submit" class="btn btn-primary" name="aksi" value="edit"><i class="fa-solid fa-check"></i> Ubah Data</button>
+        <?php
+        } else {
+        ?>
+          <button type="submit" class="btn btn-primary" value="add"><i class="fa-solid fa-plus"></i> Tambahkan Data</button>
+        <?php
+        }
+        ?>
+        <a type="button" class="btn btn-danger" href="index.php"><i class="fa-solid fa-xmark"></i> Batal</a>
+      </div>
+
+    </div>
+  </form>
 </div>
 
 <script
