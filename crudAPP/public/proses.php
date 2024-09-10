ec95beeafb8673ac11e9265402a467f50743bb5b
ProjectRavel <rafaelcodeid@gmail.com>
2024-09-11 01:53:02 +0700

    add image to database

--- crudAPP/public/proses.php
+++ crudAPP/public/proses.php
@@ -3,15 +3,21 @@ require 'connection.php';
 
 if (isset($_POST['aksi'])) {
     if ($_POST['aksi'] == "add") {
+
+
+
         $nisn = filter_input(INPUT_POST, 'nisn', FILTER_SANITIZE_SPECIAL_CHARS);
         $nama = filter_input(INPUT_POST, 'nama', FILTER_SANITIZE_SPECIAL_CHARS);
         $jeniskelamin = filter_input(INPUT_POST, 'jeniskelamin', FILTER_SANITIZE_SPECIAL_CHARS);
-        $foto = filter_input(INPUT_POST, 'foto', FILTER_SANITIZE_SPECIAL_CHARS);
+        $foto = $_FILES['foto']['name'];
         $alamat = filter_input(INPUT_POST, 'alamat', FILTER_SANITIZE_SPECIAL_CHARS);
 
+        $dir = "img/";
+        $tmpFile = $_FILES['foto']['tmp_name'];
+
+        move_uploaded_file($tmpFile, $dir . $foto);
         $sql = "INSERT INTO tb_siswa (nisn, nama_siswa, jenis_kelamin, foto_siswa, alamat)
         VALUES('$nisn', '$nama', '$jeniskelamin', '$foto', '$alamat')";
-
         $result = $conn->query($sql);
 
         if ($result) {
@@ -30,13 +36,18 @@ if (isset($_POST['aksi'])) {
 
 if (isset($_GET['hapus'])) {
     $id = $_GET['hapus'];
-    $sql = "DELETE FROM tb_siswa WHERE id_siswa=$id";
-    $result = $conn->query($sql);
+    $showall = "SELECT * FROM tb_siswa WHERE id_siswa = '$id'";
+    $resultshow = $conn->query($showall);
+    $row = $resultshow->fetch_assoc();
+    $foto = $row['foto_siswa'];
+    unlink("img/$foto");
+    $delete = "DELETE FROM tb_siswa WHERE id_siswa=$id";
+    $result = $conn->query($delete);
     if ($result) {
         header('location: index.php');
     } else {
         echo "Gagal Menghapus Data <a href='index.php'>[Home]</a>";
-        echo $sql;
+        echo $delete;
         echo $result;
     }
 }
