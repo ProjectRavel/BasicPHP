4c9e024f41bc48289602f76f2c0023e4b3968cf5
ProjectRavel <rafaelcodeid@gmail.com>
2024-09-27 01:17:42 +0700

    Add Data Feature

--- MaterialStockAPP/function.php
+++ MaterialStockAPP/function.php
@@ -11,6 +11,14 @@ $conn = mysqli_connect($host, $user, $password, $db);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
-}else{
+} else {
     echo "Connection established";
+}
+
+if (isset($_POST["addItem"])) {
+    $nama_barang = $_POST["nama_barang"];
+    $deskripsi_barang = $_POST["deskripsi_barang"];
+    $stock_barang = $_POST["stock_barang"];
+
+    $addItemSql = mysqli_query($conn, "INSERT INTO stock(namabarang, deskripsi, stock) VALUES ('$nama_barang', '$deskripsi_barang', '$stock_barang')");
 }
