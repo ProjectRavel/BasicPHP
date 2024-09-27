ec7bb33d784edf0a1802e463681140f297065467
ProjectRavel <rafaelcodeid@gmail.com>
2024-09-28 02:32:13 +0700

    Add Update Item & delete Item Features

--- MaterialStockAPP/function.php
+++ MaterialStockAPP/function.php
@@ -21,4 +21,62 @@ if (isset($_POST["addItem"])) {
     $stock_barang = $_POST["stock_barang"];
 
     $addItemSql = mysqli_query($conn, "INSERT INTO stock(namabarang, deskripsi, stock) VALUES ('$nama_barang', '$deskripsi_barang', '$stock_barang')");
+
+    if ($addItemSql) {
+        header("location:index.php");
+        exit();
+    } else {
+        echo "Error: " . $addItemSql . "<br>" . mysqli_error($conn);
+        exit();
+    }
+}
+
+if (isset($_POST["barangIn"])) {
+    $barangmasuk = $_POST["barangMasuk"];
+    $penerima = $_POST["penerima"];
+    $date = date("Y-m-d");
+    $qty = $_POST["qtyIn"];
+
+    $addNewItemSql = mysqli_query($conn, "INSERT INTO masuk (id_barang, tanggal, keterangan, qty) VALUES ('$barangmasuk', '$date' , '$penerima', '$qty')");
+
+    $checkStock = mysqli_query($conn, "SELECT * FROM stock WHERE id_barang='$barangmasuk'");
+    $checkStockData = mysqli_fetch_array($checkStock);
+
+    $stockNow = $checkStockData["stock"];
+    $newStock = $stockNow + $qty;
+
+    $updateStock = mysqli_query($conn, "UPDATE stock SET stock='$newStock' WHERE id_barang='$barangmasuk'");
+
+    if ($addNewItemSql) {
+        header("Location: masuk.php");
+        exit();
+    } else {
+        echo "Error: " . $addNewItemSql . "<br>" . mysqli_error($conn);
+        exit();
+    }
+}
+
+if (isset($_POST["barangOut"])) {
+    $barangKeluar = $_POST["barangKeluar"];
+    $penerima = $_POST["pembeli"];
+    $date = date("Y-m-d");
+    $qty = $_POST["qtyOut"];
+
+    $addNewItemSql = mysqli_query($conn, "INSERT INTO keluar (id_barang, tanggal, pembeli, total_keluar) VALUES ('$barangKeluar', '$date' , '$pembeli', '$qty')");
+
+    $checkStock = mysqli_query($conn, "SELECT * FROM stock WHERE id_barang='$barangKeluar'");
+    $checkStockData = mysqli_fetch_array($checkStock);
+
+    $stockNow = $checkStockData["stock"];
+    $newStock = $stockNow - $qty;
+
+    $updateStock = mysqli_query($conn, "UPDATE stock SET stock='$newStock' WHERE id_barang='$barangKeluar'");
+
+    if ($addNewItemSql) {
+        header("Location: keluar.php");
+        exit();
+    } else {
+        echo "Error: " . $addNewItemSql . "<br>" . mysqli_error($conn);
+        exit();
+    }
 }
